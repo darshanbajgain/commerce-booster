c48c20bfac8be1178f58f785c091c9f8c307610c
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-06 17:02:41 +0545

    style: hint section responsive mobile desktop

--- src/component/HintSection/HintSection.jsx
+++ src/component/HintSection/HintSection.jsx
@@ -14,39 +14,42 @@ const HintSection = () => {
             id: 1,
             title: "How much does it cost to finish an apartment?",
             description: "Are you standing on the threshold of your own \"M\", to which you have just picked up the keys, and with the eye...",
-            image:img1
+            image: img1
         },
         {
             id: 2,
             title: "Demolition of the load-bearing wall",
             description: "Are you planning a home renovation or renovation? Your vision is limited by the load-bearing wall, which you...",
-            image:img2
+            image: img2
         },
         {
             id: 3,
             title: "What color to paint a hall in a block of flats? ",
             description: "Matching the right paint is a real challenge. If you are wondering what color to paint your hall with, you can...",
-            image:img3
+            image: img3
         },
         {
             id: 4,
             title: "How does a concrete pump work?",
             description: "A concrete pump is currently one of the most common equipment used on construction sites. Its operation...",
-            image:img4
+            image: img4
         },
     ]
 
     return (
-        <div className="flex flex-col mt-[64px]">
-            <h1 className="font-bold text-[24px] text-center">Hints for you</h1>
-            <div className="flex flex-col px-4 mb-1">
+        <div className="flex flex-col mt-[64px] relative">
+            <div className="hidden lg:block absolute right-20 top-2">
+                <CommonLink message={messageLink} /> 
+            </div>
+            <h1 className="lg:mx-[88px] font-bold text-[24px] text-center lg:text-left">Hints for you</h1>
+            <div className="lg:mx-[88px] flex flex-col lg:flex-row lg:gap-x-5 px-4 lg:px-0 mb-1">
                 {
                     hints.map((hint) => (
                         <HintsCards key={hint.id} hint={hint} />
                     ))
                 }
             </div>
-            <div className="text-left">
+            <div className="lg:hidden text-left">
                 <CommonLink message={messageLink} />
             </div>
         </div>
