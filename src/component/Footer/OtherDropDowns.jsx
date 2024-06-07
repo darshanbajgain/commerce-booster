545c1ca6cb41392459412699ed9934d48a38226c
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-07 14:26:28 +0545

    style: footer styled

--- src/component/Footer/OtherDropDowns.jsx
+++ src/component/Footer/OtherDropDowns.jsx
@@ -1,10 +1,10 @@
 import arrowUp from "../../assets/footer_assets/keyboard_arrow_down.svg";
 
-const OtherDropDowns = ({title}) => {
+const OtherDropDowns = ({ title, children }) => {
 
     return (
         <div className=" m-0 p-4  border-gray-200 shadow-sm lg:mt-2 lg:shadow-none">
-            <div className="flex flex-row justify-between items-center">
+            <div className=" flex flex-row justify-between items-center">
                 <h3
                     className="font-bold cursor-pointer text-[16px]  lg:text-[20px]"
                 >
@@ -12,6 +12,7 @@ const OtherDropDowns = ({title}) => {
                 </h3>
                 <img className="lg:hidden" src={arrowUp} />
             </div>
+            <ul className="hidden lg:block lg:mx-0 lg:mt-4">{children}</ul>
         </div>
     )
 }
