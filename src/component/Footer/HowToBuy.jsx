b350e39a6c032034004db7855b2a264964f15686
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-10 17:20:18 +0545

    refactor: footer refactored

--- src/component/Footer/HowToBuy.jsx
+++ src/component/Footer/HowToBuy.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import arrowUp from "../../assets/footer_assets/keyboard_arrow_up.svg";
+import arrowDown from "../../assets/footer_assets/keyboard_arrow_down.svg";
 
 const HowToBuy = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
-        <div className=" m-0 pb-6 border-gray-200 shadow-sm lg:mt-6 lg:shadow-none">
-            <div className="m-4 flex flex-row justify-between items-center  lg:mt-0">
+        <div className="m-0 pb-6 border-gray-200 shadow-sm lg:mt-6 lg:shadow-none">
+            <div
+                className="m-4 flex flex-row justify-between items-center lg:mt-0"
+                onClick={() => setIsOpen(!isOpen)}
+            >
                 <h3
-                    className="font-bold cursor-pointer text-bgForOrderBtn text-[16px] lg:text-[20px] lg:text-black"
+                    className={`font-bold cursor-pointer text-[16px] lg:text-[20px] ${isOpen ? 'text-bgForOrderBtn' : 'text-black'}`}
                 >
                     {title}
                 </h3>
-                <img className="lg:hidden" src={arrowUp} />
+                <img className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`} src={arrowUp} alt="arrow up" />
+                <img className={`lg:hidden ${isOpen ? 'hidden' : 'block'}`} src={arrowDown} alt="arrow down" />
             </div>
-            <ul className="mx-4">{children}</ul>
+            {/* Always show the list on md and larger screens, toggle on smaller screens */}
+            <ul className={`mx-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
+                {children}
+            </ul>
         </div>
     );
 };
--- src/component/Footer/OtherDropDowns.jsx
+++ src/component/Footer/OtherDropDowns.jsx
@@ -1,18 +1,25 @@
-import arrowUp from "../../assets/footer_assets/keyboard_arrow_down.svg";
+import { useState } from "react";
+import arrowUp from "../../assets/footer_assets/keyboard_arrow_up.svg";
+import arrowDown from "../../assets/footer_assets/keyboard_arrow_down.svg";
 
 const OtherDropDowns = ({ title, children }) => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <div className=" m-0 p-4  border-gray-200 shadow-sm lg:mt-2 lg:shadow-none">
-            <div className=" flex flex-row justify-between items-center">
+            <div className=" flex flex-row justify-between items-center"
+                onClick={() => setIsOpen(!isOpen)}>
                 <h3
                     className="font-bold cursor-pointer text-[16px]  lg:text-[20px]"
                 >
                     {title}
                 </h3>
-                <img className="lg:hidden" src={arrowUp} />
+                <img className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`} src={arrowUp} alt="arrow up" />
+                <img className={`lg:hidden ${isOpen ? 'hidden' : 'block'}`} src={arrowDown} alt="arrow down" />
             </div>
-            <ul className="hidden lg:block lg:mx-0 lg:mt-4">{children}</ul>
+            <ul className={`lg:mx-0 mt-4 ${isOpen ? 'block' : 'hidden'} md:block`}>{children}</ul>
         </div>
     )
 }
