de15066d5d1f9210ee6974821eb0b22bb49344dc
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-11 09:13:03 +0545

    done

--- src/component/Footer/HowToBuy.jsx
+++ src/component/Footer/HowToBuy.jsx
@@ -12,7 +12,7 @@ const HowToBuy = ({ title, children }) => {
                 onClick={() => setIsOpen(!isOpen)}
             >
                 <h3
-                    className={`font-bold cursor-pointer text-[16px] lg:text-[20px] ${isOpen ? 'text-bgForOrderBtn' : 'text-black'}`}
+                    className={`font-bold cursor-pointer lg:text-black text-[16px] lg:text-[20px] ${isOpen ? 'text-bgForOrderBtn' : 'text-black'}`}
                 >
                     {title}
                 </h3>
