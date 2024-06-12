1a95eae324e8b509312fa563552efe49eedf254b
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-12 15:42:53 +0545

    feat: landing page added; also context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import AdBoxes from "./component/AdBoxes/AdBoxes";
-import AdvertiseBox from "./component/AdvertiseBox/AdvertiseBox";
-import AdvertiseMiddleBox from "./component/AdvertiseBoxMiddle/AdvertiseMiddleBox";
-import EventsComponent from "./component/EventSection/EventsComponent";
-import Footer from "./component/Footer/Footer";
-import Header from "./component/Header/Header";
-import HintSection from "./component/HintSection/HintSection";
-import RecommendedCategory from "./component/RecommendedCategory/RecommendedCategory";
-import RecommendedProduct from "./component/RecommendedProducts/RecommenedProduct";
-import ShippingDeliveryUI from "./component/ShippingUI/ShippingDeliveryUI";
+import LandingPage from "./Pages/LandingPage";
 
 export default function App() {
   return (
     <div className="font-baiserSquare font-normal text-bodycolor">
-      <Header />
-      <RecommendedProduct />
-      <AdvertiseBox />
-      <RecommendedCategory />
-      <HintSection />
-      <AdvertiseMiddleBox />
-      <AdBoxes />
-      <ShippingDeliveryUI />
-      <EventsComponent />
-      <Footer />
+      <LandingPage />
     </div>
   );
 }
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,29 @@
+import AdBoxes from "../component/AdBoxes/AdBoxes";
+import AdvertiseBox from "../component/AdvertiseBox/AdvertiseBox";
+import AdvertiseMiddleBox from "../component/AdvertiseBoxMiddle/AdvertiseMiddleBox";
+import EventsComponent from "../component/EventSection/EventsComponent";
+import Footer from "../component/Footer/Footer";
+import Header from "../component/Header/Header";
+import HintSection from "../component/HintSection/HintSection";
+import RecommendedCategory from "../component/RecommendedCategory/RecommendedCategory";
+import RecommendedProduct from "../component/RecommendedProducts/RecommenedProduct";
+import ShippingDeliveryUI from "../component/ShippingUI/ShippingDeliveryUI";
+
+const LandingPage = () => {
+  return (
+    <div className="font-baiserSquare font-normal text-bodycolor">
+      <Header />
+      <RecommendedProduct />
+      <AdvertiseBox />
+      <RecommendedCategory />
+      <HintSection />
+      <AdvertiseMiddleBox />
+      <AdBoxes />
+      <ShippingDeliveryUI />
+      <EventsComponent />
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
