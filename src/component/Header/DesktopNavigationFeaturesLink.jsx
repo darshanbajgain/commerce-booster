3fe2446180c6546ee7afd5fc4860abb68c5f9a50
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-06-12 10:50:22 +0545

    style: button adjusted

--- src/component/Header/DesktopNavigationFeaturesLink.jsx
+++ src/component/Header/DesktopNavigationFeaturesLink.jsx
@@ -6,27 +6,27 @@ const DesktopNavigationFeaturesLink = () => {
     <div className="flex items-center text-sm">
       <div className="flex flex-col items-center mr-8">
         <div className="relative">
-          <img src={CompareIcon} alt="Compare" className="w-4 h-4" />
-          <span className="absolute -top-3 -right-2 text-[10px] bg-bgForCartPop w-[18px] h-[18px] flex flex-col items-center rounded-full text-white ">
-            3
+          <img src={CompareIcon} alt="Compare" className="w-[18px] h-[18px]" />
+          <span className="absolute -top-3 -right-2 text-[10px] bg-bgForCartPop w-[18px] h-[18px]  flex flex-col items-center rounded-full text-white ">
+            <p className="leading-[14px] my-auto mx-auto">3</p>
           </span>
         </div>
         <span className=" text-PhoneColor mt-1 font-[600]">COMPARE</span>
       </div>
       <div className="flex flex-col items-center mr-5">
         <div className="relative">
-          <img src={FavoritesIcon} alt="Favorites" className="w-4 h-4" />
+          <img src={FavoritesIcon} alt="Favorites" className="w-[18px] h-[18px]" />
           <span className="absolute -top-3 -right-2 text-[10px] bg-bgForCartPop w-[18px] h-[18px] flex flex-col items-center rounded-full text-white ">
-            7
+          <p className="leading-[14px] my-auto mx-auto">7</p>
           </span>
         </div>
         <span className=" text-PhoneColor mt-1 font-[600]">FAVORITES</span>
       </div>
       <div className="flex flex-col items-center w-[116px] relative">
         <div className="relative">
-          <img src={CartIcon} alt="Cart" className="w-4 h-4" />
+          <img src={CartIcon} alt="Cart" className="w-[18px] h-[18px]" />
           <span className="absolute -top-3 -right-2 text-[10px] bg-bgForCartPop w-[18px] h-[18px] flex flex-col items-center rounded-full text-white ">
-            45
+          <p className="leading-[14px] my-auto mx-auto">45</p>
           </span>
         </div>
         <span className="text-PhoneColor mt-0 text-center font-[600]">
